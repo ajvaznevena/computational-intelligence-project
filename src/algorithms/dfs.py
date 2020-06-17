1b35ef21e26fc7ba9df4490348deac523b5d66da
ajvaznevena <nevena.ajvaz@hotmail.com>
2020-06-17 19:36:27 +0200

    Some changes in DFS algorithm

--- src/algorithms/dfs.py
+++ src/algorithms/dfs.py
@@ -8,34 +8,44 @@ class Dfs(AlgorithmInterface):
         super().__init__()
         self.ghost = ghost
         self.player = player
-        self.init = True
-        self.path = []
 
     def run(self):
         start = pixelToGrid((self.ghost.x, self.ghost.y))
         startNameKey = getNodeName(start)
 
-        visited = [startNameKey]
-        stack = [startNameKey]
-        while stack:
-            v = stack[-1]
-            if v not in visited:
-                visited.append(v)
-            removeFromStack = True
-            for n,w in graph.get_neighbors(v):
-                if n not in visited:
-                    stack.append(n)
-                    removeFromStack = False
-                    break
-            if removeFromStack:
-                stack.pop()
+        goal = pixelToGrid((self.player.x, self.player.y))
+        goalNameKey = getNodeName(goal)
 
-        return visited
+        visited = set()
+        stack = [(startNameKey, [startNameKey])]
+        while stack:
+            v,path = stack.pop()
+            if v not in visited:
+                if v == goalNameKey:
+                    return path
+                visited.add(v)
+                for n,w in graph.get_neighbors(v):
+                    stack.append((n, path+[n]))
 
 
     def getNextStep(self):
-        if self.init:
-            self.path = self.run()
-            self.init = False
+        goal = self.getGoal(self.ghost.index)
+        self.goalNameKey = getNodeName(goal)
 
-        return self.path.pop(0)
+        if self.ghost.path == [] or self.ghost.path[-1] != self.goalNameKey:
+            path = self.run()
+
+            if path is None:
+                print("Error :(")
+                sys.exit(1)
+
+            self.ghost.path = path
+            self.goalNameKey = self.ghost.path[-1] if self.ghost.path != [] else ''
+        else:
+            # if goal did not change, we dont'h have to recalculate path again
+            self.ghost.path.pop(0)
+
+        if len(self.ghost.path) == 0:
+            return None
+        else:
+            return self.ghost.path[0]
